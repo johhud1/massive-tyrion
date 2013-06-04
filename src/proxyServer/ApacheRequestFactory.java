be667ef0181f60a96c5e9172b0ba5ec3b06eefdd
johhud1 <jackinth3b0x@gmail.com>
2013-06-04 14:37:07 -0500

    added ApacheRequestFactory

---
+++ src/proxyServer/ApacheRequestFactory.java
@@ -0,0 +1,49 @@
+package proxyServer;
+
+import org.apache.http.*;
+import org.apache.http.impl.DefaultHttpRequestFactory;
+import org.apache.http.impl.entity.EntityDeserializer;
+import org.apache.http.impl.entity.LaxContentLengthStrategy;
+import org.apache.http.impl.io.AbstractSessionInputBuffer;
+import org.apache.http.impl.io.HttpRequestParser;
+import org.apache.http.io.HttpMessageParser;
+import org.apache.http.io.SessionInputBuffer;
+import org.apache.http.message.BasicHttpEntityEnclosingRequest;
+import org.apache.http.message.BasicLineParser;
+import org.apache.http.params.BasicHttpParams;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class ApacheRequestFactory {
+    public static HttpRequest create(final String requestAsString) {
+        try {
+            SessionInputBuffer inputBuffer = new AbstractSessionInputBuffer() {
+                {
+                    init(new ByteArrayInputStream(requestAsString.getBytes()), 10, new BasicHttpParams());
+                }
+
+                @Override
+                public boolean isDataAvailable(int timeout) throws IOException {
+                    throw new RuntimeException("have to override but probably not even called");
+                }
+            };
+            HttpMessageParser parser = new HttpRequestParser(inputBuffer, new BasicLineParser(new ProtocolVersion("HTTP", 1, 1)), new DefaultHttpRequestFactory(), new BasicHttpParams());
+            HttpMessage message = parser.parse();
+            if (message instanceof BasicHttpEntityEnclosingRequest) {
+                BasicHttpEntityEnclosingRequest request = (BasicHttpEntityEnclosingRequest) message;
+                EntityDeserializer entityDeserializer = new EntityDeserializer(new LaxContentLengthStrategy());
+                HttpEntity entity = entityDeserializer.deserialize(inputBuffer, message);
+                request.setEntity(entity);
+            }
+            return (HttpRequest) message;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (HttpException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
