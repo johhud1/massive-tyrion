33fa67682e1a6faa5d39adfcaf3b820333566317
johhud1 <jackinth3b0x@gmail.com>
2013-08-17 18:17:30 -0500

    switched over to sqlite for storing performance info. Much better idea. helper classes implemented

---
+++ src/Logging/PerfDBHelper.java
@@ -0,0 +1,77 @@
+package Logging;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.database.sqlite.SQLiteCursorDriver;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQuery;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.util.Log;
+
+
+
+public class PerfDBHelper extends SQLiteOpenHelper {
+    private static final String TABLE_REQUESTS = "requests";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_START_TIME = " st";
+    private static final String COLUMN_END_TIME = " et";
+    private static final String COLUMN_CONTENT_SIZE = " cs";
+    private static final String COLUMN_RES = " resource";
+    private static final String COLUMN_RES_HOST = "reshost";
+    private static final String TIME_VAR_TYPE = " INTEGER,";
+    private static final String CS_VAR_TYPE = " INTEGER,";
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table " + TABLE_REQUESTS + "("
+                                                  + COLUMN_ID
+                                                  + " integer primary key autoincrement,"
+                                                  + COLUMN_START_TIME + TIME_VAR_TYPE
+                                                  + COLUMN_END_TIME + TIME_VAR_TYPE
+                                                  + COLUMN_CONTENT_SIZE + CS_VAR_TYPE
+                                                  + COLUMN_RES_HOST + " text,"
+                                                  + COLUMN_RES + " text"
+                                                  + ");";
+
+
+    //private SQLiteDatabase db;
+    public PerfDBHelper(Context c) {
+        // change name to null for in mem db
+        super(c, "MOA_DB_" + (new Date().toLocaleString()), null, 0);
+        
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(PerfDBHelper.class.getName(), "Upgrading database from version " + oldVersion + " to "
+                                      + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_REQUESTS);
+        onCreate(db);
+    }
+    
+    public long addRequest(long start){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_START_TIME, start);
+        long id = db.insert(TABLE_REQUESTS, null, cv);
+        if(id == -1){
+            Log.e(PerfDBHelper.class.getName(), " error inserting value into db");
+        }
+        return id;
+    }
+
+}
---
+++ src/Logging/RequestInfo.java
@@ -0,0 +1,51 @@
+package Logging;
+
+public class RequestInfo {
+
+    private long id;
+    private long start;
+    private long end;
+    private int csize;
+    private String res;
+    private String host;
+
+    public RequestInfo(){
+    }
+
+    public long getId() {
+        return id;
+      }
+      public void setId(long id) {
+        this.id = id;
+      }
+      public void setStartTime(long v){
+          start = v;
+      }
+      public long getStartTime(){
+          return start;
+      }
+      public void setEndTime(long e){
+          end = e;
+      }
+      public long getEndTime(){
+          return end;
+      }
+      public void setContSize(int s){
+          csize = s;
+      }
+      public int getContSize(){
+          return csize;
+      }
+      public String getRes(){
+          return res;
+      }
+      public void setRes(String r){
+          res = r;
+      }
+      public String getHost(){
+          return host;
+      }
+      public void setHost(String h){
+          host = h;
+      }
+}
